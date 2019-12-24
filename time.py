d7841c0646c12e687f734398da5ee548cd29975a
„pchochu“ <pchochula@gmail.com>
2019-12-24 13:25:05 +0100

    The exact time

--- time.py
+++ time.py
@@ -1,8 +1,8 @@
 class Time:
     def __init__(self, h = 0, m = 0, s = 0):
-        assert 0 <= h <= 23
-        assert 0 <= m <= 59
-        assert 0 <= s <= 60
+        assert 0 <= int(h) <= 23
+        assert 0 <= int(m) <= 59
+        assert 0 <= int(s) <= 60
         
         self._hours   = int(h)
         self._minutes = int(m)
