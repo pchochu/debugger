6ae16a32a85c31023451dba3a2c39ea639eab36a
„pchochu“ <pchochula@gmail.com>
2019-12-24 13:24:32 +0100

    The exact time

---
+++ time.py
@@ -0,0 +1,29 @@
+class Time:
+    def __init__(self, h = 0, m = 0, s = 0):
+        assert 0 <= h <= 23
+        assert 0 <= m <= 59
+        assert 0 <= s <= 60
+        
+        self._hours   = int(h)
+        self._minutes = int(m)
+        self._seconds = int(s)
+                
+    def hours(self):
+        return self._hours
+
+    def minutes(self):
+        return self._minutes
+
+    def seconds(self):
+        return self._seconds
+
+    def __repr__(self):
+        return "{0:02d}:{1:02d}:{2:02d}".format(
+            self.hours(), self.minutes(), self.seconds())
+
+def test():
+    t = Time(3.14, 0, 0)
+    assert repr(t) == "03:00:00"
+
+
+
